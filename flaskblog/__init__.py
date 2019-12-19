5cef27328e21eeeceb85585cba8911da02b701f5
Leonard Thorondor <1457700790@qq.com>
2019-12-19 22:47:35 +0800

    update myblog first time

---
+++ flaskblog/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd5cb5e15d9dad92c5f284fd5b1cc6d98'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///good.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskblog import routes
